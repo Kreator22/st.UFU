145187b8c83276b02964e4fae20b194c8e0e3788
Kreator2 <kulakov_aa@list.ru>
2025-04-26 22:59:27 +1200

    Лабораторная работа #1, задача 10

---
+++ 1.Lab.10/Program.cs
@@ -0,0 +1,12 @@
+﻿//Лабораторная работа #1, задача 9.
+//https://edu.mmcs.sfedu.ru/mod/assign/view.php?id=18850
+
+//Создать метод, который возвращает истину, если целое число, переданное в качестве параметра,
+//совпадает с номером високосного года.
+
+int year = 2024;
+Console.WriteLine(IsLeapYear(year));
+year = 2025;
+Console.WriteLine(IsLeapYear(year));
+
+static bool IsLeapYear(int year) => year % 4 == 0;
