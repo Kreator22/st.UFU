8ac2b510683c340f7cbd236f5f7e93952311c330
Kreator2 <kulakov_aa@list.ru>
2025-05-03 15:35:43 +1200

    Удаление мусора

--- 01 HomeWork/ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-static void ttt2() { }
-
-public static class TTT
-{
-    public static void ttt() { }
-}
