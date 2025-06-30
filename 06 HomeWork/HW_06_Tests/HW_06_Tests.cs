5d0d8b34218a197874a5d40bf001a2a6860c77c2
Kreator2 <kulakov_aa@list.ru>
2025-06-30 22:54:27 +1200

    Домашнее задание #6, юнит тесты

---
+++ 06 HomeWork/HW_06_Tests/HW_06_Tests.cs
@@ -0,0 +1,22 @@
+﻿using HW_06;
+using HW06 = HW_06.HW_06;
+
+namespace HW_06_Tests
+{
+    [TestClass]
+    public sealed class HW_06_Tests
+    {
+        [TestMethod]
+        public void HW_06_Test()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "measurements.txt");
+            Console.WriteLine(path + Environment.NewLine);
+
+            for(int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(Environment.NewLine + $"Набор данных #{i + 1}");
+                HW06.PrintCalculations(path, i);
+            }
+        }
+    }
+}
