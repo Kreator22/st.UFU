bfc1ff219b159fc3702b446649fff9c7d553d5ed
Kreator2 <kulakov_aa@list.ru>
2025-07-05 15:55:23 +1200

    Домашнее задание #7, юнит-тесты

---
+++ 07 HomeWork/HW_07_Tests/HW_07_Tests.cs
@@ -0,0 +1,22 @@
+﻿using HW_07;
+
+namespace HW_07_Tests
+{
+    [TestClass]
+    public sealed class HW_07_Tests
+    {
+        [TestMethod]
+        public void RPN()
+        {
+            string source = "3 + 4 * 2 / ( 1 - 5 ) ^ 2";
+            string actual = HW_07.RPN.InfixToRPN(source);
+            string expected = "3 4 2 * 1 5 − 2 ^ / +";
+            StringAssert.Equals(expected, actual);
+
+            Console.WriteLine("Infix:");
+            Console.WriteLine(source);
+            Console.WriteLine("RPN:");
+            Console.WriteLine(actual + " = " + HW_07.RPN.CalculateRPN(actual));
+        }
+    }
+}
