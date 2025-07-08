0f0aec00edb0559b601ef22a6f736591fed016b8
Kreator2 <kulakov_aa@list.ru>
2025-07-09 00:53:59 +1200

    Домашнее задание #8, юнит-тесты

---
+++ 08 HomeWork/HW_08_Tests/HW_08_Tests.cs
@@ -0,0 +1,31 @@
+﻿using Lab_09;
+
+namespace HW_08_Tests
+{
+    [TestClass]
+    public sealed class HW_08_Tests
+    {
+        [TestMethod]
+        public void SerializeXML()
+        {
+            string directory = Directory.GetCurrentDirectory();
+            string fileName = "SOAP.dat";
+            string path = Path.Combine(directory, fileName);
+            Console.WriteLine(path);
+
+            HW_08.XML.SerializeXML(HW_08.XML.testBase, path);
+        }
+
+        [TestMethod]
+        public void DeSerializeXML()
+        {
+            string directory = Directory.GetCurrentDirectory();
+            string fileName = "SOAP.dat";
+            string path = Path.Combine(directory, fileName);
+            Console.WriteLine(path);
+
+            ProductDatabase database = HW_08.XML.DeSerializeXML(path);
+            Console.WriteLine(database);
+        }
+    }
+}
