0533bf3d39bff6aeba77bbfc0948a4576d83a940
Kreator2 <kulakov_aa@list.ru>
2025-07-09 00:53:15 +1200

    Домашнее задание #8. Сериализация

---
+++ 08 HomeWork/HW_08/Binary.cs
@@ -0,0 +1,37 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+
+//Бинарную сериализацию больше нельзя использовать
+//https://learn.microsoft.com/ru-ru/dotnet/standard/serialization/binaryformatter-security-guide
+
+using Lab_09;
+
+namespace HW_08
+{
+    /*static class Binary
+    {
+        static void SerializeBinary(ProductDatabase productDatabase, string path)
+        {
+            string directory = Path.GetFullPath(path);
+            if (!Directory.Exists(directory))
+                throw new DirectoryNotFoundException("Указаная директория не найдена" +
+                    Environment.NewLine + directory);
+
+            BinaryFormatter formatter = new();
+            using (FileStream fs = File.OpenWrite(path))
+                formatter.Serialize(fs, productDatabase);
+        }
+
+        static ProductDatabase DeserializeBinary(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Файл для десериализации не найден", path);
+
+            BinaryFormatter formatter = new();
+            ProductDatabase database;
+            using (FileStream fs = File.OpenRead(path))
+                database = (ProductDatabase)formatter.Deserialize(fs);
+
+            return database;
+        }
+    }*/
+}
---
+++ 08 HomeWork/HW_08/XML.cs
@@ -0,0 +1,76 @@
+﻿using Lab_09;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace HW_08
+{
+    public static class XML
+    {
+        public static ProductDatabase testBase = new([
+            new Product("Bread", 2.5m),
+            new Product("Milk", 4),
+            new Product("Onion", 0.99m)
+            ]);
+
+        /// <summary>
+        /// Создаёт сериализатор настроенный для сериализации объектов ProductDatabase
+        /// </summary>
+        /// <returns>Экземпляр сериализатора</returns>
+        static private DataContractSerializer GetProductDatabaseSerializer()
+        {
+            //Задаём для сериализируемых типов атрибуты [DataContract] и [DataMember].
+            //На основе атрибутов сериализатор составляет контракты,
+            //согласно которым будет будет происходить сериализация экземпляров типов.
+            //Указываем сериализатору перечень типов (помимо корневого) для которых надо составить контракты 
+            //и быть готовым сериализировать экземпляры.
+            Type[] knownTypes = [typeof(Product)];
+            //Создаём экземпляр сериализатора.
+            return new DataContractSerializer(typeof(ProductDatabase), knownTypes);
+        }
+
+
+        public static void SerializeXML(ProductDatabase productDatabase, string path)
+        {
+            string? directory = Path.GetDirectoryName(path);
+            if (!Directory.Exists(directory))
+                throw new DirectoryNotFoundException
+                    ("Директория не найдена" + Environment.NewLine + directory);
+
+
+            var serializer = GetProductDatabaseSerializer();
+
+
+            //Создаём поток
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+                //Для потока создаём XmlDictionaryWriter, который умеет писать XML в поток
+                using (XmlDictionaryWriter xdw = XmlDictionaryWriter.CreateTextWriter(fs, Encoding.UTF8))
+                    //А через него пишем с помощью сериализатора DataContractSerializer
+                    serializer.WriteObject(xdw, testBase);
+        }
+        
+        public static ProductDatabase DeSerializeXML(string path)
+        {
+            if (!File.Exists(path))
+                throw new FileNotFoundException("Файл не найден" + Environment.NewLine, path);
+
+            var serializer = GetProductDatabaseSerializer();
+            ProductDatabase database;
+            //Создаём поток
+            using (FileStream fs = new FileStream(path, FileMode.Open))
+            //Для потока создаём XmlDictionaryReader, который умеет читать XML из потока
+            using (XmlDictionaryReader xdr = XmlDictionaryReader.CreateTextReader(fs, new XmlDictionaryReaderQuotas()))
+                //А через него читаем и парсим объект с помощью (де)сериализатора DataContractSerializer
+                database = (ProductDatabase)serializer.ReadObject(xdr);
+
+            return database;
+        }
+
+    }
+}
