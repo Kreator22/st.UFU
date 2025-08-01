d712961577a7280159076b3bd408a75e454dfdc5
Kreator2 <kulakov_aa@list.ru>
2025-08-02 09:43:27 +1200

    Добавлено Компаратор для сравнивания экземляров ISolverFactory по имени

---
+++ 12 Lab/Lab_12/Model/Solver Factories/SolverFactoryComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_12.Model.Solver_Factories
+{
+    class SolverFactoryComparer : IEqualityComparer<ISolverFactory>
+    {
+        public bool Equals(ISolverFactory? x, ISolverFactory? y) =>
+            x is not null &&
+            y is not null &&
+            x.SolverName == y.SolverName;
+
+        public int GetHashCode([DisallowNull] ISolverFactory obj) =>
+            obj.SolverName.GetHashCode();
+    }
+}
