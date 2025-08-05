c1989145968b9bc74b63f91bb55e204659202b5f
Kreator2 <kulakov_aa@list.ru>
2025-08-05 22:53:54 +1200

    Лабораторная работа №13. Исключения

---
+++ 13 Lab/Lab_13/LimitedStringLoader.cs
@@ -0,0 +1,131 @@
+﻿using System.IO.Enumeration;
+using System.Text.RegularExpressions;
+
+namespace Lab_13
+{
+    /// <summary>
+    /// Загрузка файла с предуставновленными ограничениями
+    /// </summary>
+    public class LimitedStringLoader
+    {
+        /// <summary>
+        /// Запрещенные символы
+        /// </summary>
+        public string Prohibited { get; protected set; }
+
+        /// <summary>
+        /// Ошибочные символы
+        /// </summary>
+        public string Erroneus { get; protected set; }
+
+        /// <summary>
+        /// Ограничение на количество запрещенных символов
+        /// </summary>
+        public int ProLimit { get; protected set; }
+
+        /// <summary>
+        /// Счетчик встреченных запрещенных символов
+        /// </summary>
+        private int proTimes;
+
+        private List<string> strings;
+        public List<string> Strings {
+            get
+            {
+                if (strings is not null && strings.Any())
+                    return strings;
+                else
+                    throw new Exception("DataNotLoaded");
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="prohibited">Запрещенные символы</param>
+        /// <param name="erroneus">Ошибочные символы</param>
+        /// <param name="proLimit"></param>
+        public LimitedStringLoader(string prohibited, string erroneus, int proLimit)
+        {
+            Prohibited = prohibited;
+            Erroneus = erroneus;
+            ProLimit = proLimit;
+
+            var intersection = prohibited.ToHashSet().Intersect(erroneus.ToHashSet());
+
+            if (intersection.Any())
+                throw new InconsistentLimits(intersection);
+        }
+
+        public void Load(string fileName)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException("Файл не найден", fileName);
+
+            List<string> _strings = new();
+
+            int i = 0;
+            foreach(string line in File.ReadLines(fileName))
+            {
+                char first = line[0];
+
+                if (Prohibited.Contains(first))
+                    if (proTimes >= ProLimit)
+                        throw new TooManyProhibitedLines();
+                    else
+                        proTimes++;
+
+                else if (Erroneus.Contains(first))
+                    throw new WrongStartingSymbol(first, i);
+
+                else if (!Regex.IsMatch(first.ToString(), @"[A-Z]"))
+                    throw new IncorrectString(i);
+
+                _strings.Add(line);
+                i++;
+            }
+
+            strings = _strings;
+        }
+    }
+
+    public class InconsistentLimits : ArgumentException
+    {
+        private static string _message =
+            "В запрещённых и одинаковых символах не может быть одинаковых символов";
+        public InconsistentLimits(IEnumerable<char> symbols) : base(_message)
+        {
+            foreach (char c in symbols)
+                Data.Add(c, c);
+        }
+
+        public InconsistentLimits(char c) : base(_message) =>
+            Data.Add(c, c);
+    }
+
+    public class WrongStartingSymbol : Exception
+    {
+        private static string _message =
+            "Строка начинается с ошибочного (erroneus) символа";
+
+        public WrongStartingSymbol(char c, int stringNumber) : base(_message) =>
+            Data.Add(stringNumber, c);
+    }
+
+    public class TooManyProhibitedLines : Exception
+    {
+        private static string _message =
+            "Запрещенные (prohibited) символы встретились слишком много раз";
+
+        public TooManyProhibitedLines() : base(_message) { }
+    }
+
+    public class IncorrectString : Exception
+    {
+        private static string _message =
+            "Строка началась не с заглавной латинской буквы";
+
+        public IncorrectString(int stringNumber) : base(_message) =>
+            Data.Add(stringNumber, stringNumber);
+    }
+}
---
+++ 13 Lab/Lab_13_Tests/Lab_13_Tests.cs
@@ -0,0 +1,139 @@
+﻿using Lab_13;
+
+namespace Lab_13_Tests
+{
+    [TestClass]
+    public sealed class Lab_13_Tests
+    {
+        [TestMethod]
+        public void InconsistentLimits()
+        {
+            try
+            {
+                LimitedStringLoader loader = new("qwer", "erty", 0);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                foreach (var value in ex.Data.Values)
+                    Console.WriteLine(value.ToString());
+            }
+        }
+
+        [TestMethod]
+        public void FileNotFound()
+        {
+            LimitedStringLoader loader = new("qwe", "rty", 0);
+
+            try
+            {
+                loader.Load("qwerty");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.FileName);
+            }
+        }
+
+        [TestMethod]
+        public void DataNotLoaded()
+        {
+            LimitedStringLoader loader = new("qwe", "rty", 0);
+
+            try
+            {
+                var data = loader.Strings;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TooManyProhibitedLines()
+        {
+            List<string> strings = [
+                "A 100 100",
+                "B 200 200",
+                "B 200 200"];
+            string file = CreateTestFile(strings);
+
+            LimitedStringLoader loader = new("B", "", 1);
+            try
+            {
+                loader.Load(file);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                DeleteTestFile(file);
+            }
+        }
+
+        [TestMethod]
+        public void WrongStartingSymbol()
+        {
+            List<string> strings = [
+                "A 100 100",
+                "B 200 200",
+                "B 200 200"];
+            string file = CreateTestFile(strings);
+
+            LimitedStringLoader loader = new("", "B", 1);
+            try
+            {
+                loader.Load(file);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                DeleteTestFile(file);
+            }
+        }
+
+        [TestMethod]
+        public void IncorrectString()
+        {
+            List<string> strings = [
+                "A 100 100",
+                "B 200 200",
+                "b 200 200"];
+            string file = CreateTestFile(strings);
+
+            LimitedStringLoader loader = new("", "B", 1);
+            try
+            {
+                loader.Load(file);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                DeleteTestFile(file);
+            }
+        }
+
+        public string CreateTestFile(IEnumerable<string> strings)
+        {
+            string path = Directory.GetCurrentDirectory();
+            path = Path.Combine(path, "test.txt");
+
+            File.WriteAllLines(path, strings);
+
+            return path;
+        }
+
+        public void DeleteTestFile(string filePath) =>
+            File.Delete(filePath);
+    }
+}
